d2f75620eb152f2721c772606e85beac8db8cbce
魏翼超然 <1512763623@qq.com>
2018-12-15 23:36:10 +0800

    nug

--- cfmy-controller/src/main/java/com/zw/CfmyControllerApplication.java
+++ cfmy-controller/src/main/java/com/zw/CfmyControllerApplication.java
@@ -2,12 +2,26 @@ package com.zw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CfmyControllerApplication {
+public class CfmyControllerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CfmyControllerApplication.class, args);
+	}
+
+	/**
+	 *  extends SpringBootServletInitializer
+	 * 实现SpringBootServletInitializer可以让spring-boot项目在web容器中运行
+	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        /*UserController userController = SpringUtil.getBean(UserController.class);
+        System.out.println("拿到bean了，哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈"+userController);*/
+		builder.sources(this.getClass());
+		return super.configure(builder);
 	}
 
 }
