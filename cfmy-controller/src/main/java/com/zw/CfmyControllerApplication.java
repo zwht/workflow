6e5559c83d716e975a620ff4cf571710d1a2ccfa
魏翼超然 <1512763623@qq.com>
2018-12-23 11:35:26 +0800

    分页修改

--- cfmy-controller/src/main/java/com/zw/CfmyControllerApplication.java
+++ cfmy-controller/src/main/java/com/zw/CfmyControllerApplication.java
@@ -1,6 +1,7 @@
 package com.zw;
 
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import com.github.pagehelper.PageHelper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +10,8 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import java.util.Properties;
 
 @MapperScan("com.zw.dao.mapper")
 @SpringBootApplication
@@ -25,6 +28,19 @@ public class CfmyControllerApplication extends SpringBootServletInitializer {
             }
         };
         return customizer;
+    }
+
+    //配置mybatis的分页插件pageHelper
+    @Bean
+    public PageHelper pageHelper(){
+        PageHelper pageHelper = new PageHelper();
+        Properties properties = new Properties();
+        properties.setProperty("offsetAsPageNum","true");
+        properties.setProperty("rowBoundsWithCount","true");
+        properties.setProperty("reasonable","true");
+        properties.setProperty("dialect","mysql");    //配置mysql数据库的方言
+        pageHelper.setProperties(properties);
+        return pageHelper;
     }
 
     /**
