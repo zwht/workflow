070490eeb4286eda6ca1dd9cb779c2670b92a4fd
zhaowei <1512763623@qq.com>
2022-02-22 00:27:40 +0800

    Long to string

--- v1-common/src/main/java/com/zw/common/interceptor/MyAdapter.java
+++ v1-common/src/main/java/com/zw/common/interceptor/MyAdapter.java
@@ -1,10 +1,20 @@
 package com.zw.common.interceptor;
 
 
+import com.alibaba.fastjson.serializer.SerializeConfig;
+import com.alibaba.fastjson.serializer.ToStringSerializer;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author：zhaowei
@@ -32,5 +42,25 @@ public class MyAdapter extends WebMvcConfigurationSupport {
     public  void addResourceHandlers(ResourceHandlerRegistry registry){
         registry.addResourceHandler("/assets/**")
                 .addResourceLocations("classpath:/static/frontend/assets/");
+    }
+
+    @Override
+    public  void configureMessageConverters(List<HttpMessageConverter<?>> converters){
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        //格式化json数据格式
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        //序列化时避免精度丢失，转换为字符串
+        SerializeConfig serializeConfig = SerializeConfig.globalInstance;
+        serializeConfig.put(BigInteger.class, ToStringSerializer.instance);
+        serializeConfig.put(Long.class, ToStringSerializer.instance);
+        serializeConfig.put(Long.TYPE, ToStringSerializer.instance);
+        fastJsonConfig.setSerializeConfig(serializeConfig);
+        fastJsonConfig.setDateFormat("yyyy-HH-dd HH:mm:ss");
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        List<MediaType> fastMediaTypes = new ArrayList<>();
+        fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        fastMediaTypes.add(MediaType.APPLICATION_JSON);
+        fastConverter.setSupportedMediaTypes(fastMediaTypes);
+        converters.add(0,fastConverter);
     }
 }
